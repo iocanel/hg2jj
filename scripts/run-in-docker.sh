f9769a6e0a3957b1f2031b38ad04776d9c635da1
Ioannis Canellos <iocanel@gmail.com>
2022-07-16 17:43:39 +0300

    doc: how to run

--- scripts/run-in-docker.sh
+++ scripts/run-in-docker.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
 # This command optionall accepts the following arguments:
-# -repository:	The docker repository
-# -user:	The user under which the container is run (should match the one specified on build).
 # -version:	The version of the image.
 # 
 # If you want to use these options you need to install `readopt` and `fitleropts`. 
 # They can be found in the shell module and can be installed using `make helpers_install`.
 #
-U=`readopt -user "$@" 2> /dev/null`
 V=`readopt -version "$@" 2> /dev/null`
-A=`filteropts 2 -user -version "$@" 2> /dev/null`
+A=`filteropts 1 -version "$@" 2> /dev/null`
 
-USERNAME=${U:-"iocanel"}
+# USERNAME is the user folder where your home directory will be mounted
+USERNAME="hg2jj"
 VERSION=${V:-"latest"}
 ARGS=${A:-"$@"}
 
@@ -20,6 +18,6 @@ xhost local:root
 docker run -v ~/:/home/$USERNAME \
     	   -v /tmp/.X11-unix:/tmp/.X11-unix \
            -e DISPLAY=$DISPLAY \
-           --device=/dev/dri \
+           --device=/dev/dri \ 
            iocanel/hg2jj:$VERSION $ARGS
 
