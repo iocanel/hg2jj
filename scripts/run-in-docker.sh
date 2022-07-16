90717122b872640d2b24b88f4e8294518f0b6bde
Ioannis Canellos <iocanel@gmail.com>
2022-07-16 18:45:43 +0300

    fix: decouple run-in-docker.sh from shellib functions

--- scripts/run-in-docker.sh
+++ scripts/run-in-docker.sh
@@ -6,18 +6,15 @@
 # If you want to use these options you need to install `readopt` and `fitleropts`. 
 # They can be found in the shell module and can be installed using `make helpers_install`.
 #
-V=`readopt -version "$@" 2> /dev/null`
-A=`filteropts 1 -version "$@" 2> /dev/null`
-
 # USERNAME is the user folder where your home directory will be mounted
+
 USERNAME="hg2jj"
-VERSION=${V:-"latest"}
-ARGS=${A:-"$@"}
+VERSION="latest"
 
 xhost local:root
 docker run -v ~/:/home/$USERNAME \
+    	   -v /mnt/media:/mnt/media \
     	   -v /tmp/.X11-unix:/tmp/.X11-unix \
            -e DISPLAY=$DISPLAY \
            --device=/dev/dri \
-           iocanel/hg2jj:$VERSION $ARGS
-
+           iocanel/hg2jj:$VERSION
