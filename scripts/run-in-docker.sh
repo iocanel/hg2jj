72356d28b7cea10b9d76cc4a521393a3912a2a65
Ioannis Canellos <iocanel@gmail.com>
2023-11-20 08:02:57 +0200

    fix: docker runner script to support mpv

--- scripts/run-in-docker.sh
+++ scripts/run-in-docker.sh
@@ -13,12 +13,22 @@ VERSION="latest"
 
 xhost local:root
 docker run \
+         -v ~/.Xauthority:/home/$USERNAME/.Xauthority \
          -v ~/:/home/$USERNAME \
     	   -v /mnt/media:/mnt/media \
     	   -v /mnt/downloads:/mnt/downloads\
          -v ~/.cache/hg2jj:/opt/hg2jj/.cache \
     	   -v /tmp/.X11-unix:/tmp/.X11-unix \
+         -v /dev/:/dev/ \
+         -v /var/run/user/$(id -u)/:/var/run/user/$(id -u)/:ro \
+         -v /var/run/dbus/:/var/run/dbus \
+         -v /var/lib/dbus/:/var/lib/dbus \
+         -v /etc/machine-id/:/etc/machine-id \
          -e DISPLAY=$DISPLAY \
+         -e PULSE_SERVER=unix:/run/user/1000/pulse/native \
          -e HG2JJ_DIR=/opt/hg2jj/ \
+         --device=/dev/snd \
          --device=/dev/dri \
+         --privileged \
+         --net=host \
          iocanel/hg2jj:$VERSION
